ac08474dd7fae220fe2675fa99d64874fea45b8f
Luca Lischetti <luca@sirlisko.com>
2022-08-22 09:25:25 +0100

    fix: fixed error rendering

--- src/components/GifTile.jsx
+++ src/components/GifTile.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GifTile = ({ gifObj, dynamic }) => (
-  <div>
-    {dynamic && <img src={gifObj.still} alt={gifObj.text} />}
-    <video key={gifObj.gif} loop autoPlay muted playsInline>
-      <source src={gifObj.gif} type="video/mp4" />
-    </video>
-  </div>
-);
+const GifTile = ({ gifObj, dynamic }) =>
+  gifObj ? (
+    <div>
+      {dynamic && <img src={gifObj.still} alt={gifObj.text} />}
+      <video key={gifObj.gif} loop autoPlay muted playsInline>
+        <source src={gifObj.gif} type="video/mp4" />
+      </video>
+    </div>
+  ) : (
+    <></>
+  );
 
 GifTile.propTypes = {
   gifObj: PropTypes.shape({
